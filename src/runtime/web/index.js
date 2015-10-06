0a7f3491080977426dd229e4391e1c5c1b3427d4
Zibi Braniecki <gandalf@mozilla.com>
2015-10-06 15:46:12 -0700

    But the solution to long lines is not an empty return. Who'd guess. Now, manually tested with grunt lint

--- src/runtime/web/index.js
+++ src/runtime/web/index.js
@@ -64,8 +64,8 @@ document.l10n = {
         return Promise.resolve(navigator.mozL10n.qps['qps-ploc'].name);
       },
       processString: function(s) {
-        return
-          Promise.resolve(navigator.mozL10n.qps['qps-ploc'].translate(s));
+        return Promise.resolve(
+          navigator.mozL10n.qps['qps-ploc'].translate(s));
       }
     },
     'qps-plocm': {
@@ -73,8 +73,8 @@ document.l10n = {
         return Promise.resolve(navigator.mozL10n.qps['qps-plocm'].name);
       },
       processString: function(s) {
-        return
-          Promise.resolve(navigator.mozL10n.qps['qps-plocm'].translate(s));
+        return Promise.resolve(
+          navigator.mozL10n.qps['qps-plocm'].translate(s));
       }
     }
   },
